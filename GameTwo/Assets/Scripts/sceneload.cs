842d81e85c0227f9cc64b569ecdac9a3a21dcb14
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-01-20 16:13:11 -0500

    downloaded some sprites for the game, added a background image to level one.

---
+++ GameTwo/Assets/Scripts/sceneload.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneload : MonoBehaviour
+{
+    public void Loadscene()
+    {
+
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+
+    }
+
+
+    public void Loadstart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+}
+
+
+
+
+
+
+
+
