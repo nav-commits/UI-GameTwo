57241e269802d2a43b4598993a56a095b85fa100
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-01-21 16:27:49 -0500

    added some lines of code for the paddle to move with the mouse

---
+++ GameTwo/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWidthUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float mouseposUnits = Input.mousePosition.x / Screen.width * screenWidthUnits;
+       Vector2 paddlepos = new Vector2(mouseposUnits, transform.position.y);
+       transform.position = paddlepos;
+    }
+}
