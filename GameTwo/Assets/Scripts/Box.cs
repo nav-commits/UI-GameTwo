7ce7daead12a851a27f63ac957a3098a2318a4d9
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-02 13:58:47 -0500

    improved level two by adding balls, boxes and new logic

--- GameTwo/Assets/Scripts/Box.cs
+++ GameTwo/Assets/Scripts/Box.cs
@@ -36,7 +36,6 @@ public class Box : MonoBehaviour
     }
 
 
-
     private void YouWin()
 
     {
--- GameTwo/Assets/Scripts/Leveltwo.cs
+++ GameTwo/Assets/Scripts/Leveltwo.cs
@@ -10,19 +10,12 @@ public class Leveltwo : MonoBehaviour
     public GameObject [] PlayerScore;
     public Text ScoreText;
     public int score;
-    public int scoretwo;
-    public Text ScoreTextTwo;
-    public GameObject[] PlayerScoreTwo;
-   
 
     void Start()
 
     {
-        score = 5;
+        score = 9;
         ScoreText.text = "Player Score:" + score.ToString();
-
-        scoretwo = 0;
-        ScoreTextTwo.text = "Player Score:" + scoretwo.ToString();
 
     }
 
@@ -32,35 +25,42 @@ public class Leveltwo : MonoBehaviour
        
     }
 
+
     public void DestoryScore()
     {
        
         score--;
 
-        if (score < 5)
+        for (int i = 0; i > score; i++)
         {
-            PlayerScore[0].SetActive(false);
+            PlayerScore[i].SetActive(false);
+            Debug.Log("HELLO");
         }
 
-        if (score < 4)
-        {
-            PlayerScore[1].SetActive(false);
-        }
+        //if (score < 5)
+        //{
+        //    PlayerScore[0].SetActive(false);
+        //}
 
-        if (score < 3)
-        {
-            PlayerScore[2].SetActive(false);
-        }
+        //if (score < 4)
+        //{
+        //    PlayerScore[1].SetActive(false);
+        //}
 
-        if (score < 2)
-        {
-            PlayerScore[3].SetActive(false);
-        }
+        //if (score < 3)
+        //{
+        //    PlayerScore[2].SetActive(false);
+        //}
 
-        if (score < 1)
-        {
-            PlayerScore[4].SetActive(false);
-        }
+        //if (score < 2)
+        //{
+        //    PlayerScore[3].SetActive(false);
+        //}
+
+        //if (score < 1)
+        //{
+        //    PlayerScore[4].SetActive(false);
+        //}
 
 
         ScoreText.text = "Player Score:" + score.ToString();
@@ -69,51 +69,6 @@ public class Leveltwo : MonoBehaviour
         {
             GameOver();
         }
-    }
-
-
-    public void Points()
-    {
-        scoretwo++;
-
-        if (scoretwo > 0)
-        {
-            PlayerScoreTwo[0].SetActive(true);
-        }
-
-        if (scoretwo > 1)
-        {
-            PlayerScoreTwo[1].SetActive(true);
-        }
-
-        if (scoretwo > 2)
-        {
-            PlayerScoreTwo[2].SetActive(true);
-        }
-
-        if (scoretwo > 3)
-        {
-            PlayerScoreTwo[3].SetActive(true);
-        }
-
-        if (scoretwo > 4)
-        {
-            PlayerScoreTwo[4].SetActive(true);
-        }
-
-        if (scoretwo > 5)
-        {
-            PlayerScoreTwo[5].SetActive(true);
-        }
-
-
-
-        if (scoretwo == 5)
-        {
-            WinGame();
-        }
-
-        ScoreTextTwo.text = "Player Score:" + scoretwo.ToString();
     }
 
 
