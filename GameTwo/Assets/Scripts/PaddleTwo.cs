47fb4299dca2f1b95dd63a52187d68e5627823b2
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-02-03 17:42:29 -0500

    fixing min and max width of the paddle

--- GameTwo/Assets/Scripts/PaddleTwo.cs
+++ GameTwo/Assets/Scripts/PaddleTwo.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PaddleTwo : MonoBehaviour
 {
     [SerializeField] float screenWidthunits = 16f;
+    [SerializeField] float minX = -2f;
+    [SerializeField] float minY = 8f;
    
     void Start()
     {
@@ -16,6 +18,7 @@ public class PaddleTwo : MonoBehaviour
     {
         float Movemouse = Input.mousePosition.x / Screen.width * screenWidthunits;
         Vector2 paddlepos = new Vector2(Movemouse, transform.position.y);
+        paddlepos.x = Mathf.Clamp(Movemouse, minX, minY);
         transform.position = paddlepos;
     }
 }
